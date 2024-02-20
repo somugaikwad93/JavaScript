756c399e241fc1602e566c20fd42d77e18020b35
somanathg <somugaikwad93@gmail.com>
2024-02-20 14:03:16 +0530

    Added basic array concepts

---
+++ arrays.js
@@ -0,0 +1,18 @@
+let lang = ["JAVA","JavaScript","Python"]
+console.log(lang)
+
+// 1. push() : It is used to add an extra element at the end of the array
+lang.push("C#");
+console.log(lang)
+
+// 2. pop() : It is used to remove a last element from an array
+lang.pop()
+console.log(lang)
+
+// 3. shift() : Removes the first element from an array and returns it.
+lang.shift()
+console.log(lang)
+
+// 4. unshift() : It is used to add an element at the start of an array
+lang.unshift("JAVA")
+console.log(lang)
