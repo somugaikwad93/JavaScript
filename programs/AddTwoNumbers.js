e6d80e92fbc935eced8d910adefe2ac3eb95fc64
somanathg <somugaikwad93@gmail.com>
2024-03-20 15:39:08 +0530

    New

---
+++ programs/AddTwoNumbers.js
@@ -0,0 +1,20 @@
+// By using + operator
+let num1 = 10;
+let num2 = 20;
+console.log(num1 + num2)
+
+//using function
+function add(a, b){
+    console.log(a+b)
+}
+add(20, 30)
+
+//using arrow func
+let sum = (a, b) => { return a+b}
+console.log(sum(1, 2))
+
+//using += operator
+let n1 = 20;
+let n2 = 40;
+n1 += n2
+console.log(n1)
---
+++ programs/MultiplicationOfTwoNumbers.js
@@ -0,0 +1,21 @@
+// using * operator
+let num1 = 2;
+let num2 = 3;
+console.log(num1 * num2)
+
+//using function
+function multiply(a,b){
+    console.log(a*b);
+}
+multiply(2,3)
+
+//using arrow function
+let multiplication = (a,b) => { return a*b}
+console.log(multiplication(2,3));
+
+//using *= operator
+let n1 = 2;
+let n2 = 3;
+n1 *= n2;
+console.log(n1)
+
