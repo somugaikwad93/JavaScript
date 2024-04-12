7dc783a5473e129df4300330ab53c6cea6eb2e6d
somugaikwad93 <somugaikwad93@gmail.com>
2024-04-12 13:46:49 +0530

    SwitchCondition

---
+++ Concepts/SwitchCondition.js
@@ -0,0 +1,32 @@
+function demoSwitch(a){
+    var day = "";
+    switch (a) {
+        case 1:
+            day = 'Sunday'
+            break;
+        case 2:
+            day = "Monday"
+            break;
+        case 3:
+            day = "Tuesday"
+            break;
+        case 4:
+            day = "Wednesday"
+            break;
+        case 5:
+            day = "Thursday"
+            break;
+        case 6:
+            day = "Friday"
+            break;
+        case 7:
+            day = "Saturday"
+            break;
+        default:
+            day = "Incorrect value...Please enter correct value"
+            break;
+    }
+    return day;
+}
+
+console.log(demoSwitch(2));
